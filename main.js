ca6aba02e053e3ca6616f0ee5d98a29190bc3883
caiohenriquedeoliveira <caioholiveira13@gmail.com>
2015-10-26 10:20:08 -0200

    Adicionado arquivo jquery e corrigido evento keyup para mostrar o nome digitado

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 $( document ).ready(function() {
-    
-
 	$('#campo-nome').keyup(function(){
-
+	console.log("tecla solta");
 		// Obter o nome digitado e o atribuir na variável abaixo
 		var nomeDigitado = '';
+		nomeDigitado = $("#campo-nome").val();
 
 		// O ID nome-digitado deveria estar recebendo o nome digitado pelo usuário, isto não está acontecendo, 
 		// verifique o log de erros no navegador e o corrija.
-		$('#nome-digitado').text(nomedigitado);
+		$('#nome-digitado').text(nomeDigitado);
 
 
 		// A variável abaixo deve conter a quantidade de caracteres do nome digitado, crie a lógica para obter este valor
