552abfac3ba01fd7e763894d377b38677eece663
caiohenriquedeoliveira <caioholiveira13@gmail.com>
2015-10-28 00:30:40 -0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 $( document ).ready(function() {
+	// Capturando as teclas digitadas pelo usuario detro do elemento "campo-nome"
 	$('#campo-nome').keyup(function(){
-		// Obter o nome digitado e o atribuir na variável abaixo
-		var nomeDigitado = '';
+		// Capturando o texto digitado pelo usuario no campo e atribuindo para a variável
+		var nomeDigitado = ''; 
 		nomeDigitado = $("#campo-nome").val();
 
-		// O ID nome-digitado deveria estar recebendo o nome digitado pelo usuário, isto não está acontecendo, 
-		// verifique o log de erros no navegador e o corrija.
+		// Imprimindo o texto digitado pelo usuario no elemento "nome-digitado"
 		$('#nome-digitado').text(nomeDigitado);
 
-		// A variável abaixo deve conter a quantidade de caracteres do nome digitado, crie a lógica para obter este valor
+		// Contando a quantidade de caracteres e atribuindo o valor para a variável "quantidadeCaracteres"
 		var quantidadeCaracteres = 0;
 		quantidadeCaracteres = nomeDigitado.length;
 
-		// Em seguida informe este valor no elementdo com ID quantidade-caracteres
+		// Imprimindo a quantidade de caracteres no elemento "quantidade-caracteres"
 		$("#quantidade-caracteres").text(quantidadeCaracteres);
 	});
 
